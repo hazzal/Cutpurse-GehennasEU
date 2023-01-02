2de5c83b6ea2d0d50d22d594051c7be7b7e58363
hazzal <3193549+hazzal@users.noreply.github.com>
2023-01-02 21:10:44 +0100

    Convert to Gehennas namespace

--- settings.lua
+++ settings.lua
@@ -5,25 +5,25 @@ local _, t = ...
 
 -- Set this to be your addon name. This *has to be* the same name as your root addon
 -- folder and the same name as in your Table of Contents (.toc) file!
-t.addon_name = "Cutpurse-TestList"
+t.addon_name = "Cutpurse-GehennasEU"
 
 -- Set this variable to whatever realm your list is for.
 -- e.g. Golemagg, Firemaw, Gehennas etc.
-t.my_realm = "Classic PTR Realm 1"
+t.my_realm = "Gehennas"
 
 -- Set this variable to the name of your organisation, usually the name of your
 -- Discord.
 -- e.g. "Golemagg EU Discord"
-t.my_provider = "Cutpurse Discord"
+t.my_provider = "Gehennas Discord"
 
 -- Set this variable to be the name of your list, can just be set to the name of
 -- your organisation if you only curate a single list.
-t.my_name = "Cutpurse Discord"
+t.my_name = "Gehennas Discord"
 
 -- Set this variable to provide a description of your organisation.
 -- e.g. "Realm discord for the Golemagg EU server."
-t.my_description = "Test Addon to demonstrate Cutpurse extension lists."
+t.my_description = "Realm discord for the Gehennas EU server"
 
 -- Set this variable to be a url to your discord or website where people
 -- can come and find you.
-t.my_url = "https://some_discord_url.com/whatever"
+t.my_url = "https://discord.gg/gehennas"
